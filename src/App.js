1de08e2d1f6e03f9d96605c4a95394245be5085b
riteshdev07 <rkdevop07@gmail.com>
2024-07-24 17:37:41 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+ 
 function App() {
+  const [city, setCity] = useState("Delhi");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+ 
+  const currentDate = new Date();
+ 
+  const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+ 
+  const month = months[currentDate.getMonth()];
+  const day = currentDate.getDate();
+  const year = currentDate.getFullYear();
+ 
+  const formattedDate = `${month} ${day}, ${year}`;
+ 
+  const API_KEY = "bcda10ba323e88e96cb486015a104d1d"; 
+ 
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      const data = await response.json();
+      console.log(data)
+      // if (response.ok) {
+      setWeatherData(data);
+     
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      
+    }
+  };
+ 
+  useEffect(()=>{
+ 
+     
+   
+ 
+  fetchWeatherData();
+ 
+  },[])
+ 
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchWeatherData();
+  };
+ 
+  const getWeatherIconUrl = (main) => {
+    switch (main) {
+      case "Clear":
+        return "/sun.png"; 
+      case "Rain":
+        return "/rain_with_cloud.png";    
+      case "Snow":
+        return "/Tornado.png"; 
+      case "Haze":
+        return "/thunder.png"; 
+     
+      default:
+        return null;
+    }
+  };
+ 
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+ 
+      <div className="container">
+        {weatherData && (
+          <>
+            <h1 className="container_date">{formattedDate}</h1>
+            <div className="weather_data">
+              <h2 className="container_city">{weatherData.name}</h2>
+             { <img className="container_img" src="/thunder.png" width="180px" alt="sss"/> }
+              <img className="container_img" src={getWeatherIconUrl(weatherData.weather[0].main)} width="180px" alt=""/>       
+              <h2 className="container_degree">{weatherData.main.temp}</h2>
+              <h2 className="country_per">{weatherData.weather[0].main}<span className="degree_icon"></span></h2>
+              <form className="form" onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  class="input"
+                  placeholder="Enter city name"
+                  value={city}
+                  onChange={handleInputChange}
+                  required
+                />
+                <button type="submit">Get</button>
+              </form>
+            </div>
+          </>
+        )}
+ 
+ 
+      </div>
     </div>
   );
 }
-
+ 
 export default App;
